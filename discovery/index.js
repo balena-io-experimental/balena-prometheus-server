cc93eb87c81e6c4faf9d1f2c318ab02d39c0aef1
Aric Huang <arichuang@gmail.com>
2018-07-02 18:57:23 -0700

    fix query

--- discovery/index.js
+++ discovery/index.js
@@ -38,9 +38,9 @@ function format(device) {
     labels: {
       resin_device_uuid: device.uuid,
       resin_app: process.env.RESIN_APP_NAME,
-      name: device.name,
-      os_version: device.os_version,
-      commit: device.is_on__commit,
+      resin_device_name: device.name,
+      resin_device_os_version: device.os_version,
+      resin_device_commit: device.is_on__commit,
     }
   }
 }
